772d22d4fd1bd5c6adc12e9d74602a1a7aedad63
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-03-08 15:33:16 +0100

    Add constraints to the model

--- program.py
+++ program.py
@@ -8,3 +8,21 @@ for r in range(3):
     for c in range(4):
         x[(r,c)] = model.new_int_var(1, 12, f"x_{r}_{c}")
 
+# starting values constraint
+model.add(x[(0,1)] == 6)    # B1
+model.add(x[(1,0)] == 8)    # A2
+model.add(x[(2,2)] == 3)    # C3
+
+# every value different constraint
+model.add_all_different(x.values())
+
+# the sum of rows constraint
+model.add(x[(0,0)] + x[(0,1)] + x[(0,2)] + x[(0,3)] == 30)
+model.add(x[(1,0)] + x[(1,1)] + x[(1,2)] + x[(1,3)] == 18)
+model.add(x[(2,0)] + x[(2,1)] + x[(2,2)] + x[(2,3)] == 30)
+
+# the sum of columns constraint
+model.add(x[(0,0)] + x[(1,0)] + x[(2,0)] == 27)
+model.add(x[(0,1)] + x[(1,1)] + x[(2,1)] == 16)
+model.add(x[(0,2)] + x[(1,2)] + x[(2,2)] == 10)
+model.add(x[(0,3)] + x[(1,3)] + x[(2,3)] == 25)
