42f76770ec350525f663388c90a473f57cc2574c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-03-10 18:01:18 +0100

    Use different model

--- program.py
+++ program.py
@@ -1,38 +1,36 @@
-from ortools.sat.python import cp_model
+from ortools.linear_solver import pywraplp
 
-model = cp_model.CpModel()
+solver = pywraplp.Solver.CreateSolver('SCIP')
 
 # vars declaration 3x4
 x = {}
 for r in range(3):
     for c in range(4):
-        x[(r,c)] = model.new_int_var(1, 12, f"x_{r}_{c}")
+        x[(r,c)] = solver.IntVar(1, 12, f"x_{r}_{c}")
 
 # starting values constraint
-model.add(x[(0,1)] == 6)    # B1
-model.add(x[(1,0)] == 8)    # A2
-model.add(x[(2,2)] == 3)    # C3
-
-# every value different constraint
-model.add_all_different(x.values())
+solver.Add(x[(0,1)] == 6)    # B1
+solver.Add(x[(1,0)] == 8)    # A2
+solver.Add(x[(2,2)] == 3)    # C3
 
 # the sum of rows constraint
-model.add(x[(0,0)] + x[(0,1)] + x[(0,2)] + x[(0,3)] == 30)
-model.add(x[(1,0)] + x[(1,1)] + x[(1,2)] + x[(1,3)] == 18)
-model.add(x[(2,0)] + x[(2,1)] + x[(2,2)] + x[(2,3)] == 30)
+solver.Add(x[(0,0)] + x[(0,1)] + x[(0,2)] + x[(0,3)] == 30)
+solver.Add(x[(1,0)] + x[(1,1)] + x[(1,2)] + x[(1,3)] == 18)
+solver.Add(x[(2,0)] + x[(2,1)] + x[(2,2)] + x[(2,3)] == 30)
 
 # the sum of columns constraint
-model.add(x[(0,0)] + x[(1,0)] + x[(2,0)] == 27)
-model.add(x[(0,1)] + x[(1,1)] + x[(2,1)] == 16)
-model.add(x[(0,2)] + x[(1,2)] + x[(2,2)] == 10)
-model.add(x[(0,3)] + x[(1,3)] + x[(2,3)] == 25)
+solver.Add(x[(0,0)] + x[(1,0)] + x[(2,0)] == 27)
+solver.Add(x[(0,1)] + x[(1,1)] + x[(2,1)] == 16)
+solver.Add(x[(0,2)] + x[(1,2)] + x[(2,2)] == 10)
+solver.Add(x[(0,3)] + x[(1,3)] + x[(2,3)] == 25)
 
-solver = cp_model.CpSolver()
-status = solver.solve(model)
+status = solver.Solve()
 
-if status == cp_model.OPTIMAL:
+if status == pywraplp.Solver.OPTIMAL:
     for r in range(3):
         row = []
         for c in range(4):
-            row.append(solver.Value(x[(r,c)]))
+            row.append(int(x[(r,c)].solution_value()))
         print(row)
+else:
+    "No solution!"
